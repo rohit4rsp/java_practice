dfa176eb741222d2271c52724654f4033e1af717
rohit4rsp <80252100+rohit4rsp@users.noreply.github.com>
2021-07-02 21:13:08 +0530

    Create CWH_78_Errors_Exceptions.java
    errors

---
+++ firstjavaprogramWH/src/com/company/CWH_78_Errors_Exceptions.java
@@ -0,0 +1,19 @@
+package com.company;
+          //BASICS
+/*There are three types of errors in java....
+        1) Syntax errors
+        2) Logical errors
+        3) Runtime errors - also called Exceptions*/
+
+public class CWH_78_Errors_Exceptions {
+    public static void main(String[] args) {
+
+        int a = 5;
+//        int a = 5      // No semicolon, syntax errors!
+        int b = 9;
+//        b = 9;         // Variable not declared, syntax errors
+        System.out.println(a+b);
+
+
+    }
+}
